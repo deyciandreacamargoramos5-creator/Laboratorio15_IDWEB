eeb037b15d89a5f81d2d3277ec533cc96ba84bb7
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-11-19 19:54:15 -0500

    Ejercicio 3

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// 1. Ejercicio 1
-console.log("Documento cargado correctamente.");
-// 2. Ejercicio 2
-console.log("Título del documento:", document.title);
-// 3. Ejercicio 3
-const primerH2 = document.querySelector("h2");
-console.log("Texto del primer h2:", primerH2.textContent);
-// 4. Ejercicio 4
-const parrafos = document.querySelectorAll("p");
-parrafos.forEach(p => p.textContent = "Texto actualizado dinámicamente"); 
+// 1. Ejercicio 3
+const primerH2= document.querySelector("h2");
+console.log ("Texto del primer h2:", primerH2.textContent);
